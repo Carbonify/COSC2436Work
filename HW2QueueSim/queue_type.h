f1550e0d14a0a381bd9f9210c42df8281a043da7
Noah M <noahcodes@protonmail.com>
2021-09-27 08:40:35 -0500

    Minor fixes
    Use initializeQueue to set values to 0, as we do this a few times
    throughout the header file

--- HW2QueueSim/queue_type.h
+++ HW2QueueSim/queue_type.h
@@ -12,21 +12,18 @@ using namespace std;
 template <class Type>
 class queueType : public queueADT<Type>
 {
-  //the implementation of these funcs should really be separated out into
-  //a cpp file, with just the declarations here, but eh
 public:
   queueType(int queueSize = 100)
   {
     maxQueueSize = (queueSize > 1)? queueSize : 100;
-    count = 0;
-    queueFront = 0;
-    queueRear = 0;
+    initializeQueue();
     list = new Type[maxQueueSize];
   }
 
+
   ~queueType() {
-    //All other members are on stack and will be auto-freed
     delete[] list;
+    list = nullptr;
   }
 
   bool isEmptyQueue() const {
@@ -45,7 +42,12 @@ public:
     return maxQueueSize;
   }
 
-  void initializeQueue() {} //Unused, but must be declared because C++
+  //resets numerical values to 0, does not reallocate list
+  void initializeQueue() {
+    count = 0;
+    queueFront = 0;
+    queueRear = 0;
+  }
 
 
   Type front() const {
@@ -76,11 +78,7 @@ public:
     if(isEmptyQueue()) {
       printf("Cannot remove from an empty queue");
     } else if (count == 1) { //removing last element of list
-
-      //restore values to defaults
-      count = 0;
-      queueFront = 0;
-      queueRear = 0;
+      initializeQueue();
 
       printf("Client removed from queue. Queue now has %d clients.", count);
     } else {
