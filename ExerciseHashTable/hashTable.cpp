1960a5e6bc577b92c260ef1b579bdc1eafb2a036
Noah M <noahcodes@protonmail.com>
2021-10-07 15:34:59 -0500

    Implement inserter, fix constructor of HT exercise

--- ExerciseHashTable/hashTable.cpp
+++ ExerciseHashTable/hashTable.cpp
@@ -1,13 +1,16 @@
-#include <iostream>
+#include <cstdio>
 #include "hashTable.h"
 
-hashTable::hashTable ( int tableSize )
+hashTable::hashTable ( int tableSize ): count(0)
 {
   // implement constructor
   int fixedTableSize = (tableSize >= 1)? tableSize : 100;
   puppies = new Puppy[fixedTableSize];
-  count = 0;
 
+  //set all puppies' age to -1, so we know when spots are empty
+  for(int i = 0; i<fixedTableSize; i++) {
+    puppies[i].age = -1;
+  }
 }
 
 hashTable::~hashTable ()
@@ -25,13 +28,18 @@ int hashTable::length() const
 void hashTable::insert ( const Puppy &newItem )
 {
   // implement function
+  if(puppies[newItem.age].age != -1) { //valid insert
+    puppies[newItem.age] = newItem;
+  } else {
+    printf("Slot %d already occupied, cannot add Puppy\n", newItem.age);
+  }
 }
 
 void hashTable::printAll () const
 {
   for ( auto i = 0; i < count; i++ ) {
     if ( puppies[i].age != -1 ) {
-      std::cout << puppies[i].name << " Age " << puppies[i].age << endl;
+      printf("%s Age %d\n", puppies[i].name.c_str(), puppies[i].age);
     }
   }
 }
