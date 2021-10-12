e553cd4c019b20c64a23ccbd7fb68e27924dc837
Noah M <noahcodes@protonmail.com>
2021-10-12 13:34:13 -0500

    Add initial files, makefile

---
+++ HW3HashTables/HashTable.cpp
@@ -0,0 +1,36 @@
+#include "HashTable.h"
+#include <iostream>
+
+HashTable::HashTable() {
+	bucketArray = new HashBucket[10];
+	maxBuckets = 10;
+}
+
+HashTable::~HashTable() {
+	//free memory related to the table
+}
+
+int HashTable::hashFunc(string password) {
+	int hashval = 0;
+	for (unsigned int i = 0; i < password.size(); i++) {
+		hashval = hashval + ((char)password[i]);
+	}
+	hashval = hashval % 10;
+	return hashval;
+}
+
+int HashTable::numElementsInBucket(int bucketIndex) {
+	//return number of passwords in specified bucket of hash table
+}
+
+void HashTable::addElement(string password, bool actualPassword) {
+	//add password to correct bucket in hash table; also, indicate if password was from the actualPassword.txt file or attemptedPassword.txt file
+}
+
+void HashTable::removeElements() {
+	//print all nodes in buckets consisting of only one element, then delete that element
+}
+
+void HashTable::printCollisions() {
+	//print all compromised passwords
+}
