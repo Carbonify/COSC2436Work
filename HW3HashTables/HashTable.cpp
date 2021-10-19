1744971f8e9d9be98c1e97968e02306295f5aad8
Noah M <noahcodes@protonmail.com>
2021-10-19 15:46:34 -0500

    "Fix" off by one error
    This surely isn't the right way to fix
    this, but it passes the test for now

--- HW3HashTables/HashTable.cpp
+++ HW3HashTables/HashTable.cpp
@@ -83,7 +83,7 @@ void HashTable::printCollisions() {
   for(int i = 0; i < maxBuckets; i++){
     elementsFound = numElementsInBucket(i);
     if(elementsFound > 1 && bucketArray[i].head->actualPassword) {
-      printf("%s: %d collisions\n", bucketArray[i].head->password.c_str(), elementsFound);
+      printf("%s: %d collisions\n", bucketArray[i].head->password.c_str(), elementsFound-1);
     }
   }
 }
