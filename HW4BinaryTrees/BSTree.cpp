0c7f6b5b96fb34ead215224a8b0f70be0eb4f5f2
Noah M <noahcodes@protonmail.com>
2021-11-08 10:32:04 -0600

    Untabify BSTree

--- HW4BinaryTrees/BSTree.cpp
+++ HW4BinaryTrees/BSTree.cpp
@@ -1,83 +1,80 @@
 #include "BSTree.h"
 #include "BSTNode.h"
+#include <string>
 
 
 BSTree::BSTree()
 {
-	rootNode = nullptr;
+  rootNode = nullptr;
 }
 
 BSTree::~BSTree()
 {
-	//implement destructor
+  //implement destructor
 
 }
 
 int countHeight(BSTNode *start)
 {
-	int retd;
-	if (nullptr == start)
-	{
-		return -1;
-	}
-	else
-	{
-		int ld = countHeight(start->getLeftChild());
-		int rd = countHeight(start->getRightChild());
-		
-		return max(ld, rd) + 1;
-	}
+  int retd;
+  if (nullptr == start)
+  {
+    return -1;
+  }
+  else
+  {
+    int ld = countHeight(start->getLeftChild());
+    int rd = countHeight(start->getRightChild());
+
+    return max(ld, rd) + 1;
+  }
 
 }
 
 
-
 int BSTree::getHeight() const
 {
-	return countHeight(rootNode);
+  return countHeight(rootNode);
 }
 
 
 int countNodes(BSTNode* start)
 {
-	if (nullptr == start)
-	{
-		return 0;
-	}
-	else
-	{
-		return 1 + countNodes(start->getLeftChild()) + countNodes(start->getRightChild());
-	}
+  if (nullptr == start)
+  {
+    return 0;
+  }
+  else
+  {
+    return 1 + countNodes(start->getLeftChild()) + countNodes(start->getRightChild());
+  }
 }
 
 int BSTree::getNumNodes() const
 {
-	return countNodes(rootNode);
+  return countNodes(rootNode);
 }
-
 
 
 string BSTree::getRepresentation()
 {
-	//implement function
+  //implement function
 }
 
 BSTNode* BSTree::getNode(string key)
 {
-	//implement function
+  //implement function
 }
 
 
 bool BSTree::addNode(string key)
 {
-	BSTNode* newNode = new BSTNode(key);
-	addNode(newNode);
-	return true;
+  BSTNode* newNode = new BSTNode(key);
+  addNode(newNode);
+  return true;
 }
 
 bool BSTree::addNode(BSTNode* item)
 {
-	//implement function
+  //implement function
 }
-
-
