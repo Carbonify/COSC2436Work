e99ce5de472cff0bd7a35ac141593544a3e23a98
Noah M <noahcodes@protonmail.com>
2021-11-29 11:34:13 -0600

    Fix comments and includes

--- HW5Graphs/Graph.hpp
+++ HW5Graphs/Graph.hpp
@@ -1,8 +1,7 @@
 #pragma once
-#include <iostream>
-#include <climits>
 #include <vector>
-#include <stack>
+#include <stack> //for DFS
+#include <queue> //for MST
 
 class Graph {
 
@@ -14,13 +13,13 @@ private:
 public:
   Graph(const int mySize) : size(mySize) {
     using namespace std;
-    //Implement function
+    //Create two vectors, set insides to 0 and false respectively
     matrix = vector<vector<int>>(mySize, vector<int>(mySize, 0));
     visited = vector<bool>(mySize, false);
   }
 
   void resetVisited() {
-    //Implement function
+    //Set all values to false
     for(unsigned long i = 0; i < visited.size(); i++) {
       visited.at(i) = false;
     }
@@ -29,7 +28,7 @@ public:
   void insertEdge(const int v1, const int v2, const int weight) {
     //Implement function
     matrix.at(v1).at(v2) = weight;
-    matrix.at(v2).at(v1) = weight;
+    matrix.at(v2).at(v1) = weight; //backwards as well because undirected
   }
 
   void DFS(const int vertex) {
@@ -45,8 +44,7 @@ public:
       if (!visited.at(currentV)) {
         visited.at(currentV) = true;
 
-        //for each vertex adjV adjacent to currentV
-        //Push adjV to stack
+        //for each vertex adjV adjacent to currentV, push adjV to stack
         for(const int& v : matrix.at(currentV)) {
           if (v > 0) dfsStack.push(v);
         }
@@ -55,7 +53,7 @@ public:
   }
 
   bool edgeCheck() {
-    //Implement function
+    //True if all values in visited are true
     for(const auto& x : visited) {
       if (!x) {
         return false;
